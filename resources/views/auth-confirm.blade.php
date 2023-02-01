8095e6542a3d5e742d9233aa1519bd9f91460f3d
Venansiusfw <20.k1.0001@student.unika.com>
2023-02-01 20:27:20 +0700

    login&register

---
+++ resources/views/auth-confirm.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.template_login')
+@section('login')
+<div class="wrapper vh-100">
+    <div class="row align-items-center h-100">
+      <form class="col-lg-3 col-md-4 col-10 mx-auto text-center">
+        <div class="mx-auto text-center my-4">
+          <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="/">
+            <img class="logo" src="assets/images/logo.png" alt="asiabangunabadi"/>
+          </a>
+          <h4 class="my-3">Password reset successfully!</h4>
+        </div>
+        <div class="alert alert-success" role="alert"> An email has been sent to your email <strong>email@example.com</strong>. Please check your email to get reset link </div>
+        <a href="/" class="btn btn-lg btn-primary btn-block">Back to home</a>
+        <p class="mt-5 mb-3 text-muted">© 2020</p>
+      </form>
+    </div>
+  </div>
+  @endsection
---
+++ resources/views/auth-login-half.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.template_login')
+@section('login')
+<div class="wrapper vh-100">
+    <div class="row align-items-center h-100">
+      <div class="col-lg-6 d-none d-lg-flex">
+      </div> <!-- ./col -->
+      <div class="col-lg-6">
+        <div class="w-50 mx-auto">
+          <form class="mx-auto text-center">
+            <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="/">
+              <img class="logo" src="assets/images/logo.png" alt="asiabangunabadi"/>
+            </a>
+            <h1 class="h6 mb-3">Sign in</h1>
+            <div class="form-group">
+              <label for="inputEmail" class="sr-only">Email address</label>
+              <input type="email" id="inputEmail" class="form-control form-control-lg" placeholder="Email address" required="" autofocus="">
+            </div>
+            <div class="form-group">
+              <label for="inputPassword" class="sr-only">Password</label>
+              <input type="password" id="inputPassword" class="form-control form-control-lg" placeholder="Password" required="">
+            </div>
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me"> Stay logged in </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Let me in</button>
+          </form>
+        </div> <!-- .card -->
+      </div> <!-- ./col -->
+    </div> <!-- .row -->
+  </div>
+@endsection
