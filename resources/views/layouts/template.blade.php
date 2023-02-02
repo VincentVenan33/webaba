08451399a1e1fa79cf98e4789a7c5068e2a3ae00
unknown <ehny.soendari@gmail.com>
2023-02-02 22:55:57 +0700

    update blade menu

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -173,7 +173,7 @@
                             <span class="ml-3 item-text">User</span>
                         </a>
                         <ul class="collapse list-unstyled pl-4 w-100" id="profile">
-                            <a class="nav-link pl-3" href="user_data"><span class="ml-1">Add User</span></a>
+                            <a class="nav-link pl-3" href="{{route('tampiluser')}}"><span class="ml-1">Add User</span></a>
                             <a class="nav-link pl-3" href="./profile-settings.html"><span class="ml-1">Settings</span></a>
                             <a class="nav-link pl-3" href="./profile-security.html"><span class="ml-1">Security</span></a>
                             <a class="nav-link pl-3" href="./profile-notification.html"><span class="ml-1">Notifications</span></a>
