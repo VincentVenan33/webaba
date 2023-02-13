a7ca902aa1b36d2faa62c353b01a7ed30d9a1552
Venansiusfw <20.k1.0001@student.unika.com>
2023-02-13 22:55:51 +0700

    coba hash password belum sukses

--- app/Http/Controllers/UserdataController.php
+++ app/Http/Controllers/UserdataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserdataModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserdataController extends Controller
 {
--- resources/views/user/viewuserdata.blade.php
+++ resources/views/user/viewuserdata.blade.php
@@ -1,37 +1,5 @@
 @extends('layouts.template')
 @section('main')
-{{-- <div class="main-content">
-    <div class="user-bg shadow-lg rounded">
-        <h3>User Data</h3>
-        <a class="btn btn-success" href="{{route('adduser')}}"><i class="fa-solid fa-square-plus"></i> Add User</a><br><br>
-        <table class="table table-bordered table-hover scroll-table">
-        <tr class="">
-            <th width="100px">No.</th>
-            <th>Nama</th>
-            <th>Username</th>
-            <th>E-mail</th>
-            <th>Permission</th>
-            <th>Status</th>
-            <th>Action</th>
-        </tr>
-        @php $no = 1; @endphp
-        @foreach($user as $usr)
-        <tr>
-            <th>{{ $no++ }}</th>
-            <td>{{$usr->nama}}</td>
-            <td>{{$usr->username}}</td>
-            <td>{{$usr->email}}</td>
-            <td>{{$usr->permission}}</td>
-            <td>{{($usr->status ==1 ? "AKTIF" : "NON AKTIF")}}</td>
-            <td>
-            <a href="change/{{$usr->id}}" class="btn btn-warning btn-sm"><i class="fa fa-pencil"></i></a>
-            <a href="delete/{{$usr->id}}" onclick="return confirm('Apakah Anda Yakin Menghapus Data?');" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-            </td>
-        </tr>
-        @endforeach
-        </table>
-    </div>
-</div> --}}
 <main role="main" class="main-content">
     <div class="container-fluid">
       <div class="row justify-content-center">
