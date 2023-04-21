7cf9bd04e0afc0662cb07753e4cd7603d0320aec
Venansiusfw <20.k1.0001@student.unika.com>
2023-04-21 21:28:37 +0700

    membuat api dan sudah succes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProdukController;
+use App\Http\Controllers\Api\KatalogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/user', App\Http\Controllers\Api\UserController::class);
+Route::apiResource('/produk', App\Http\Controllers\Api\ProdukController::class);
+Route::apiResource('/katalog', App\Http\Controllers\Api\KatalogController::class);
+Route::apiResource('/team', App\Http\Controllers\Api\TeamController::class);
