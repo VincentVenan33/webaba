7b87cbb1f01ad7cd22e012a636d970d26c9060ce
Venansiusfw <20.k1.0001@student.unika.com>
2023-04-23 17:47:16 +0700

    benerin format image dan file di api data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,6 +9,7 @@ use App\Models\KatalogModel;
 use App\Models\TeamModel;
 use App\Models\Team;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 
@@ -21,16 +22,28 @@ class PostController extends Controller
     }
     public function getProduct(){
         $posts = ProdukModel::select('*')->get();
+        foreach ($posts as $post) {
+            $post->image = url('images/' . $post->image);
+        }
         return new PostResource(true,'List Data Product',$posts);
     }
 
     public function getCatalog(){
         $posts = KatalogModel::select('*')->get();
+        foreach ($posts as $post) {
+            $post->image = url('images/' . $post->image);
+        }
+        foreach ($posts as $post) {
+            $post->file = url('files/' . $post->file);
+        }
         return new PostResource(true,'List Data Catalog',$posts);
     }
 
     public function getTeam(){
         $posts = TeamModel::select('*')->get();
+        foreach ($posts as $post) {
+            $post->image = url('images/' . $post->image);
+        }
         return new PostResource(true,'List Data Team',$posts);
     }
 
