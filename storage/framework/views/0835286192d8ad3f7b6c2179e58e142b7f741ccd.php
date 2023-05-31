76ff99799486f5efb1c6552562895e791d300758
VincentVenan33 <92298629+VincentVenan33@users.noreply.github.com>
2023-05-31 20:21:17 +0700

    ngilangin menu yang ndak perlu

--- storage/framework/views/0835286192d8ad3f7b6c2179e58e142b7f741ccd.php
+++ storage/framework/views/0835286192d8ad3f7b6c2179e58e142b7f741ccd.php
@@ -44,9 +44,7 @@
                   </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Profile</a>
-                  <a class="dropdown-item" href="#">Settings</a>
-                  <a class="dropdown-item" href="#">Activities</a>
+                  
                   <a class="dropdown-item" href="<?php echo e(route('actionlogout')); ?>">Log Out</a>
                 </div>
               </li>
